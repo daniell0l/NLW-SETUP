3c51190b719b557f956b7793f1ee3c092ae1bbbb
daniell0l <daniel_l0l@hotmail.com>
2023-01-16 23:50:44 -0300

    Feat: created  query in the prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,24 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import cors from "@fastify/cors";
 
 const app = fastify();
+const prisma = new PrismaClient()
 
-app.get("/", () => {
-    return "hello word"
+app.register(cors)
+
+app.get("/", async () => {
+    const habits = await prisma.habit.findMany(
+        // {
+        //     where: {
+        //         title: {
+        //             startsWith: "acorda"
+        //         }
+        //     }
+        // }
+    )
+
+    return habits
 })
 
 app.listen({ port: 3333 }, () => console.log("server is running"));
